3d1f89aeb8d31a2d9485c2309724ae20e66f3974
Isak Hansen <isakhansen@Isaks-MacBook-Air.local>
2025-09-15 20:10:06 +0200

    nvim keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,3 +7,15 @@ vim.api.nvim_set_keymap("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true }
 vim.api.nvim_set_keymap("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "<leader>P", ":TypstPreview<CR>", { silent = true })
 vim.keymap.set("n", "<leader>e", "<cmd>lua require('mini.files').open()<CR>", { silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<Leader>fh",
+  ':lua require"telescope.builtin".find_files({ hidden = true })<CR>',
+  { noremap = true, silent = true }
+)
+-- vim.keymap.set(
+--   "n",
+--   "<leader>fh",
+--   ":lua require('telescope.builtin').live_grep({ additional_args = function() return { '--hidden' } end })<cr>",
+--   { silent = true, desc = "Find in Files" }
+-- )
