8be10e244915eb8cb47500b975dc3c180011453c
Isak Hansen <isakhansen@Isaks-MacBook-Air.local>
2025-10-14 16:34:35 +0200

    stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,17 +2,21 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
 vim.o.signcolumn = "yes"
-vim.o.tabstop = 4
+vim.o.tabstop = 2
 vim.o.swapfile = false
 vim.o.winborder = "rounded"
 vim.g.mapleader = " "
+vim.o.smartindent = true
+vim.o.undofile = true
+vim.o.termguicolors = true
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>n', ':nohl<CR>')
 
-vim.keymap.set({'n', 'v', 'x'}, '<leader>y', '"+y<CR>')
-vim.keymap.set({'n', 'v', 'x'}, '<leader>d', '"+d<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 
 vim.pack.add({
 	{ src = "https://github.com/catppuccin/nvim" },
@@ -21,22 +25,28 @@ vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
 	{ src = "https://github.com/christoomey/vim-tmux-navigator" },
+	{ src = "https://github.com/Saghen/blink.cmp" },
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('my.lsp', {}),
-	callback = function(args)
-		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-		if client:supports_method('textDocument/completion') then
-			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
-			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-			client.server_capabilities.completionProvider.triggerCharacters = chars
-			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-		end
-	end,
-})
-vim.cmd [[set completeopt+=menuone,noselect,popup]]
+-- fuzzy = { implementation = "prefer_rust_with_warning" }
+-- fuzzy.prebuilt_binaries.force_version = true
+-- fuzzy.implementation = "rust"
 
+-- vim.api.nvim_create_autocmd('LspAttach', {
+-- 	group = vim.api.nvim_create_augroup('my.lsp', {}),
+-- 	callback = function(args)
+-- 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+-- 		if client:supports_method('textDocument/completion') then
+-- 			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
+-- 			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+-- 			client.server_capabilities.completionProvider.triggerCharacters = chars
+-- 			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+-- 		end
+-- 	end,
+-- })
+-- vim.cmd [[set completeopt+=menuone,noselect,popup]]
+
+require "blink.cmp".setup()
 require "mini.pick".setup()
 require("oil").setup({
 	lsp_file_methods = {
@@ -60,6 +70,12 @@ vim.keymap.set('n', '<leader>e', ":Oil --float<CR>")
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 vim.lsp.enable({ "lua_ls", "tinymist", "ts_ls" })
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = { workspace = { library = { vim.api.nvim_get_runtime_file("", true) } }
+		}
+	}
+})
 
 vim.cmd("colorscheme catppuccin")
 vim.cmd(":hi statusline guibg=NONE")
