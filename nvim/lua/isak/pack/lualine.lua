2a085cfe160457d9afb9da8705e1e5d33c19ae5a
Isak Hansen <isakhansen@Isaks-MacBook-Air.local>
2025-10-15 21:28:17 +0200

    nice

--- nvim/lua/isak/pack/lualine.lua
+++ nvim/lua/isak/pack/lualine.lua
@@ -1,11 +1,19 @@
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-	 	-- theme = "16color",
+		theme = "mycatppuccin",
 		component_separators = { left = '', right = '' },
 		section_separators = { left = '', right = '' },
 	},
 	sections = {
 		lualine_c = { 'buffers' },
+		lualine_x = { 'encoding', 'fileformat', 'filetype',
+			{
+				require("noice").api.statusline.mode.get,
+				cond = require("noice").api.statusline.mode.has,
+				color = { fg = "#ff9e64" },
+			}
+
+		},
 	},
 })
