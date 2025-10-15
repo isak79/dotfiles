491d1022b4ae5fe201627321dcca1c833e18fbad
Isak Hansen <isakhansen@Isaks-MacBook-Air.local>
2025-10-15 19:32:46 +0200

    lualine

---
+++ nvim/lua/isak/pack/lualine.lua
@@ -0,0 +1,11 @@
+require("lualine").setup({
+	options = {
+		icons_enabled = true,
+		theme = "catppuccin",
+		component_separators = { left = '', right = '' },
+		section_separators = { left = '', right = '' },
+	},
+	sections = {
+		lualine_c = { 'buffers' },
+	},
+})
