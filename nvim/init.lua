af8de3dacad77a01b335f2ea58f75de0912cd8a4
Isak Hansen <isakhansen@Isaks-MacBook-Air.local>
2025-10-15 18:55:10 +0200

    yes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,2 @@
 require("isak.pack")
 require("isak.core")
-
-
--- require "blink.cmp".setup({
--- 	fuzzy = { implementation = "lua" },
--- 	keymap = { preset = 'default' },
--- 	completion = { documentation = { auto_show = false } },
--- 	appearance = {
--- 		nerd_font_variant = 'mono',
--- 	},
--- })
-
-
