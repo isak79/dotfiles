146e8d749d3608e3eb506a113d4573711130289c
Isak Hansen <isakhansen@Isaks-MacBook-Air.local>
2025-10-17 00:42:34 +0200

    hmm

--- nvim/lua/isak/pack/lsp.lua
+++ nvim/lua/isak/pack/lsp.lua
@@ -1,10 +1,22 @@
--- require("lspconfig").setup()
 vim.lsp.enable({ "lua_ls", "tinymist", "ts_ls", "rust-analyzer" })
 vim.lsp.config("lua_ls", {
 	settings = {
 		Lua = {
 			diagnostics = {
 				globals = { "vim" } }
+		}
+	}
+})
+
+vim.lsp.config('rust-analyzer', {
+
+	cmd = { 'rust-analyzer' },
+	filetypes = { 'rs' },
+	settings = {
+		['rust-analyzer'] = {
+			diagnostics = {
+				enable = false,
+			}
 		}
 	}
 })
