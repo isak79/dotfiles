74698fdbdab3739dd340b16c70490190fd6a9dae
Isak Hansen <isakhansen@Isaks-MacBook-Air.local>
2025-10-16 16:54:39 +0200

    ja

--- nvim/lua/isak/core/keymaps.lua
+++ nvim/lua/isak/core/keymaps.lua
@@ -1,30 +1,35 @@
 -- General
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>', {silent = true})
-vim.keymap.set('n', '<leader>w', ':write<CR>', {silent = true})
-vim.keymap.set('n', '<leader>q', ':quit<CR>', {silent = true})
-vim.keymap.set('n', '<Esc>', ':nohl<CR>', {silent = true})
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>', {silent = true})
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>', {silent = true})
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>', { silent = true })
+vim.keymap.set('n', '<leader>w', ':write<CR>', { silent = true })
+vim.keymap.set('n', '<leader>q', ':quit<CR>', { silent = true })
+vim.keymap.set('n', '<Esc>', ':nohl<CR>', { silent = true })
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>', { silent = true })
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>', { silent = true })
 
 -- Explore
-vim.keymap.set('n', '<leader> ', ":FzfLua files<CR>", {silent = true})
-vim.keymap.set('n', '<leader>/', ":FzfLua grep_visual<CR>", {silent = true})
-vim.keymap.set('n', '<leader>,', ":FzfLua buffers<CR>", {silent = true})
-vim.keymap.set('n', '<leader>f', ":Pick files<CR>", {silent = true})
-vim.keymap.set('n', '<leader>h', ":Pick help<CR>", {silent = true})
-vim.keymap.set('n', '<leader>e', ":Oil<CR>", {silent = true})
+vim.keymap.set('n', '<leader> ', ":FzfLua files<CR>", { silent = true })
+vim.keymap.set('n', '<leader>/', ":FzfLua grep_visual<CR>", { silent = true })
+vim.keymap.set('n', '<leader>,', ":FzfLua buffers<CR>", { silent = true })
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>", { silent = true })
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>", { silent = true })
+vim.keymap.set('n', '<leader>e', ":Oil<CR>", { silent = true })
 
 -- Git
-vim.keymap.set('n', '<leader>gg', ":LazyGit<CR>", {silent = true})
-vim.keymap.set('n', '<leader>gl', ":LazyGitLog<CR>", {silent = true})
-vim.keymap.set('n', '<leader>nd', ":Noice dismiss<CR>", {silent = true})
+vim.keymap.set('n', '<leader>gg', ":LazyGit<CR>", { silent = true })
+vim.keymap.set('n', '<leader>gl', ":LazyGitLog<CR>", { silent = true })
+vim.keymap.set('n', '<leader>nd', ":Noice dismiss<CR>", { silent = true })
 
 -- Buffers
-vim.keymap.set('n', 'H', ':bprev<CR>', {silent = true})
-vim.keymap.set('n', 'L', ':bnext<CR>', {silent = true})
-vim.keymap.set('n', '<leader>bd', ':bd<CR>', {silent = true})
+vim.keymap.set('n', 'H', ':bprev<CR>', { silent = true })
+vim.keymap.set('n', 'L', ':bnext<CR>', { silent = true })
+vim.keymap.set('n', '<leader>bd', ':bd<CR>', { silent = true })
 
 -- Misc
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, {silent = true})
-vim.keymap.set('n', '<leader>tp', ":TypstPreview<CR>", {silent = true})
-vim.keymap.set('n', '<leader>nn', ":Noice fzf<CR>", {silent = true})
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { silent = true })
+vim.keymap.set('n', '<leader>tp', ":TypstPreview<CR>", { silent = true })
+vim.keymap.set('n', '<leader>nn', ":Noice fzf<CR>", { silent = true })
+
+-- Lsp
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { silent = true })
+vim.keymap.set("n", "gd", ":FzfLua lsp_definitions<CR>", { silent = true })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
