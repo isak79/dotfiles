c54cace3b1406ad100e8e6d26ac598797cb5d2b1
Isak Hansen <isakhansen@Isaks-MacBook-Air.local>
2025-10-17 18:26:25 +0200

    misc

--- nvim/after/ftplugin/haskell.lua
+++ nvim/after/ftplugin/haskell.lua
@@ -1,9 +1,11 @@
--- local set = vim.opt_local
-
 vim.api.nvim_command("TSBufEnable highlight")
 
-vim.keymap.set('n', '<leader>P', function()
+vim.keymap.set('n', '<leader>Pp', function()
 	local filename = vim.fn.bufname('%')
 	os.execute("tmux split-window 'ghci " .. filename .. "; read'")
 end, { silent = true, buffer = true })
 
+
+vim.keymap.set('n', '<leader>PP', function()
+	os.execute("tmux split-window 'ghci *; read'")
+end, { silent = true, buffer = true })
