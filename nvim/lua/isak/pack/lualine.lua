6629ec64858906fc73d30344f5f87660dac45b0c
Isak Hansen <isakhansen@Isaks-MacBook-Air.local>
2025-10-19 23:15:01 +0200

    updates

--- nvim/lua/isak/pack/lualine.lua
+++ nvim/lua/isak/pack/lualine.lua
@@ -1,18 +1,31 @@
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "mycatppuccin",
+		theme = require("isak.pack.lualine_theme"),
 		component_separators = { left = '', right = '' },
 		section_separators = { left = '', right = '' },
 	},
 	sections = {
 		lualine_c = {
 			{
-				'buffers',
-				max_length = 40,
-				symbols = {
-					alternate_file = '',
-				},
+				function()
+					local bufname_list = vim.split(vim.fn.bufname('%'), '/')
+					return bufname_list[#bufname_list]
+				end,
+				cond = function ()
+					return vim.bo.modified
+				end,
+				color = { fg = "#f5a97f" }
+			},
+			{
+				function()
+					local bufname_list = vim.split(vim.fn.bufname('%'), '/')
+					return bufname_list[#bufname_list]
+				end,
+				cond = function ()
+					return not vim.bo.modified
+				end,
+				color = { fg = "" }
 			},
 		},
 		lualine_x = { 'filetype',
@@ -20,7 +33,7 @@ require("lualine").setup({
 				function()
 					local output = SysCB and " " or " "
 					return output
-	 			end
+				end
 			},
 		},
 		lualine_y = { 'lsp_status' },
---
+++ nvim/lua/isak/pack/lualine_theme.lua
@@ -0,0 +1,63 @@
+local C = {
+	rosewater = "#f4dbd6",
+	flamingo = "#f0c6c6",
+	pink = "#f5bde6",
+	mauve = "#c6a0f6",
+	red = "#ed8796",
+	maroon = "#ee99a0",
+	peach = "#f5a97f",
+	yellow = "#eed49f",
+	green = "#a6da95",
+	teal = "#8bd5ca",
+	sky = "#91d7e3",
+	sapphire = "#7dc4e4",
+	blue = "#8aadf4",
+	lavender = "#b7bdf8",
+	text = "#cad3f5",
+	subtext1 = "#b8c0e0",
+	subtext0 = "#a5adcb",
+	overlay2 = "#939ab7",
+	overlay1 = "#8087a2",
+	overlay0 = "#6e738d",
+	surface2 = "#5b6078",
+	surface1 = "#494d64",
+	surface0 = "#363a4f",
+	base = "#24273a",
+	mantle = "#1e2030",
+	crust = "#181926",
+}
+
+local transparent_bg = false and "NONE" or "#1E1E2D"
+
+return {
+	replace = {
+		a = { bg = C.red, fg = C.base, gui = "bold" },
+		b = { bg = C.surface0, fg = C.red },
+	},
+	inactive = {
+		a = { bg = transparent_bg, fg = C.blue },
+		b = { bg = transparent_bg, fg = C.surface1, gui = "bold" },
+		c = { bg = transparent_bg, fg = C.overlay0 },
+	},
+	normal = {
+		a = { bg = C.blue, fg = C.mantle, gui = "bold" },
+		b = { bg = C.surface0, fg = C.blue },
+		c = { bg = transparent_bg, fg = C.text },
+	},
+	insert = {
+		a = { bg = C.green, fg = C.base, gui = "bold" },
+		b = { bg = C.surface0, fg = C.green },
+	},
+	terminal = {
+		a = { bg = C.green, fg = C.base, gui = "bold" },
+		b = { bg = C.surface0, fg = C.green },
+	},
+	command = {
+		a = { bg = C.peach, fg = C.base, gui = "bold" },
+		b = { bg = C.surface0, fg = C.peach },
+	},
+	visual = {
+		a = { bg = C.mauve, fg = C.base, gui = "bold" },
+		b = { bg = C.surface0, fg = C.mauve },
+	},
+}
