dfb617999d9a6ade8c75bcc1af9c9304f569c430
Isak Hansen <isakhansen@Isaks-MacBook-Air.local>
2025-10-22 18:03:27 +0200

    added click feature to sketchybar spaces

--- sketchybar/plugins/spaces.sh
+++ sketchybar/plugins/spaces.sh
@@ -12,9 +12,9 @@ for sid in $(aerospace list-workspaces --all); do
     icon.padding_left=7 \
     icon.padding_right=7 \
     background.color=0x40ffffff \
-    background.corner_radius=5 \
-    background.height=25 \
+    background.height=20 \
     label.drawing=off \
+		click_script="aerospace workspace $sid" \
     script="$PLUGIN_DIR/aerospacer.sh $sid"
 done
 
