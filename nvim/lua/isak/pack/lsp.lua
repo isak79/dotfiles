72cd483fb76e47111a7bdb807b5307ac531035d6
Isak Hansen <isakhansen@Isaks-MacBook-Air.local>
2025-10-22 15:55:13 +0200

    added bash to vim lsp, trying to make spaces in sketchybar

--- nvim/lua/isak/pack/lsp.lua
+++ nvim/lua/isak/pack/lsp.lua
@@ -1,4 +1,4 @@
-vim.lsp.enable({ "lua_ls", "tinymist", "ts_ls", "rust-analyzer", "hls" })
+vim.lsp.enable({ "lua_ls", "tinymist", "ts_ls", "rust-analyzer", "hls", "bashls" })
 vim.lsp.config("lua_ls", {
 	settings = {
 		Lua = {
--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-# The $SELECTED variable is available for space components and indicates if
-# the space invoking this script (with name: $NAME) is currently selected:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
+# sketchybar --add event aerospace_workspace_change
+# sketchybar --set "$NAME" background.drawing="$SELECTED"
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+##### Adding Mission Control Space Indicators #####
+for sid in "$(aerospace list-workspaces --all)"; do
+  spaces=(
+    space="$sid"
+    icon="$sid"
+    icon.padding_left=7
+    icon.padding_right=7
+    background.color=0x40ffffff
+    background.corner_radius=5
+    background.height=25
+    label.drawing=off
+    # script="$PLUGIN_DIR/space.sh"
+  )
+  sketchybar --add space space."$sid" left
+done
