007f25ca4be843d30bee69630dec3323ce924756
Isak Hansen <isakhansen@hackerman.local>
2026-01-23 16:35:19 +0100

    added keymap for opening and closing qf list

--- nvim/lua/isak/core/keymaps.lua
+++ nvim/lua/isak/core/keymaps.lua
@@ -1,5 +1,4 @@
 -- General
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>', { silent = true })
 vim.keymap.set('n', '<leader>w', ':write<CR>', { silent = true })
 vim.keymap.set('n', '<leader>q', ':quit<CR>', { silent = true })
 vim.keymap.set('n', '<Esc>', ':nohl<CR>', { silent = true })
@@ -10,41 +9,41 @@ vim.keymap.set('v', '<C-k>', ":<C-u>silent! '<,'>m '<-2<CR>gv", { silent = true 
 vim.keymap.set('n', 'ø', "}", { silent = true })
 vim.keymap.set('n', 'Ø', "{", { silent = true })
 vim.keymap.set('n', '<leader>R', ":restart<CR>", { silent = true })
-vim.keymap.set('n', '<leader>u', function ()
+vim.keymap.set('n', '<leader>u', function()
 	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end, { silent = true })
 vim.keymap.set('n', '+', '<C-w>+', { silent = true })
 vim.keymap.set('n', '+', '<C-w>+', { silent = true })
 
 -- Explore
-vim.keymap.set('n', '<leader> ', function ()
+vim.keymap.set('n', '<leader> ', function()
 	Snacks.picker.smart()
 end, { silent = true })
-vim.keymap.set('n', '<leader>,', function ()
+vim.keymap.set('n', '<leader>,', function()
 	Snacks.picker.buffers()
 end, { silent = true })
-vim.keymap.set('n', '<leader>/', function ()
+vim.keymap.set('n', '<leader>/', function()
 	Snacks.picker.grep()
 end, { silent = true })
-vim.keymap.set('v', '<leader>/', function ()
+vim.keymap.set('v', '<leader>/', function()
 	Snacks.picker.grep_word()
 end, { silent = true })
-vim.keymap.set('n', '<leader>ff', function ()
+vim.keymap.set('n', '<leader>ff', function()
 	Snacks.picker.lsp_symbols()
 end, { silent = true })
-vim.keymap.set('n', '<leader>fr', function ()
+vim.keymap.set('n', '<leader>fr', function()
 	Snacks.picker.registers()
 end, { silent = true })
-vim.keymap.set('n', '<leader>fq', function ()
+vim.keymap.set('n', '<leader>fq', function()
 	Snacks.picker.qflist()
 end, { silent = true })
-vim.keymap.set('n', '<leader>fd', function ()
+vim.keymap.set('n', '<leader>fd', function()
 	Snacks.picker.diagnostics()
 end, { silent = true })
-vim.keymap.set('n', '<leader>fl', function ()
+vim.keymap.set('n', '<leader>fl', function()
 	Snacks.picker.lines()
 end, { silent = true })
-vim.keymap.set('n', '<leader>fh', function ()
+vim.keymap.set('n', '<leader>fh', function()
 	Snacks.picker.help()
 end, { silent = true })
 vim.keymap.set('n', '<leader>e', ":Oil<CR>", { silent = true })
@@ -53,17 +52,32 @@ vim.keymap.set('n', '<leader>e', ":Oil<CR>", { silent = true })
 vim.keymap.set('n', 'å', ":cnext<CR>", { silent = true })
 vim.keymap.set('n', 'Å', ":cprev<CR>", { silent = true })
 vim.keymap.set('n', '<leader>å', ":cexpr([])<CR>", { silent = true })
+vim.keymap.set('n', '<leader>o', function()
+	local isOpen = false
+	for _, win in pairs(vim.fn.getwininfo()) do
+		if win.quickfix == 1 then
+			isOpen = true
+		end
+	end
+
+	if isOpen then
+		vim.cmd("cclose")
+		else vim.cmd("copen")
+	end
+end)
+
+
 --Flash
 vim.keymap.set({ "n", "x", "o" }, 's', require("flash").jump)
 vim.keymap.set({ "n", "x", "o" }, 'S', require("flash").treesitter)
-vim.keymap.set( "o", 'r', require("flash").remote)
+vim.keymap.set("o", 'r', require("flash").remote)
 vim.keymap.set("c", '<C-s>', require("flash").toggle)
 
 -- Git
-vim.keymap.set('n', '<leader>gg', function ()
+vim.keymap.set('n', '<leader>gg', function()
 	Snacks.lazygit()
 end, { silent = true })
-vim.keymap.set('n', '<leader>gB', function ()
+vim.keymap.set('n', '<leader>gB', function()
 	Snacks.gitbrowse()
 end, { silent = true })
 vim.keymap.set('n', '<leader>gb', ":Gitsigns blame<CR>", { silent = true })
@@ -96,7 +110,7 @@ vim.keymap.set('n', '<leader>nd', ":Noice dismiss<CR>", { silent = true })
 
 -- Lsp
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { silent = true })
-vim.keymap.set("n", "gd", function ()
+vim.keymap.set("n", "gd", function()
 	Snacks.picker.lsp_definitions()
 end, { silent = true })
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
