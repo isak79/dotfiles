4b46259080f4fc7155ee54670d8f0dad152f1d39
Isak Hansen <isakhansen@hackerman.local>
2026-01-24 18:42:46 +0100

    updated haskell config, so that spaces are inserted before code, instead of tab

--- nvim/after/ftplugin/haskell.lua
+++ nvim/after/ftplugin/haskell.lua
@@ -1,3 +1,8 @@
+vim.bo.expandtab = true
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.softtabstop = 2
+
 vim.keymap.set('n', '<C-p><C-p>', function()
 	local filename = vim.fn.bufname('%')
 	os.execute("tmux split-window 'ghci " .. filename .. "; read'")
