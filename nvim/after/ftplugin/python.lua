82a4369fbb101287f79df5adb34e0c3c24655c42
Isak Hansen <isakhansen@hackerman.local>
2026-01-27 11:39:38 +0100

    yes

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,13 +1,4 @@
-local function get_root_dir()
-	local workspace_folder = vim.lsp.buf.list_workspace_folders()
-	if workspace_folder and #workspace_folder > 0 then
-		return workspace_folder[1]
-	end
-	return nil
-end
-
 vim.keymap.set('n', '<C-p><C-p>', function()
-	local worspace_dir = get_root_dir()
 	local bufName = vim.fn.bufname('%')
 	os.execute("tmux split-window 'python3 " .. bufName .. "; read'")
 end, { silent = true, buffer = true })
