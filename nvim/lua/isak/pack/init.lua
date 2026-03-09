9477bd5eddd4f87a3f7d7f6ac75e2b5f1a53a510
Isak Hansen <isakhansen@hackerman.local>
2026-03-09 12:12:52 +0100

    removed nvim-jdtls

--- nvim/lua/isak/pack/init.lua
+++ nvim/lua/isak/pack/init.lua
@@ -13,7 +13,6 @@ vim.pack.add({
 	{ src = "https://github.com/folke/flash.nvim" },
 	{ src = "https://github.com/nvim-mini/mini.surround.git" },
 	{ src = "https://github.com/folke/snacks.nvim" },
-	{ src = "https://github.com/mfussenegger/nvim-jdtls" },
 })
 
 require("snacks").setup({
