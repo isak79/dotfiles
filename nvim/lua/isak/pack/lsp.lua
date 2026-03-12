7fbd34111de22fe784ac411dd0aaa9169de5377b
Isak Hansen <isakhansen@hackerman.local>
2026-03-12 15:05:56 +0100

    removed hls from nvim config

--- nvim/lua/isak/pack/lsp.lua
+++ nvim/lua/isak/pack/lsp.lua
@@ -1,4 +1,4 @@
-vim.lsp.enable({ "lua_ls", "tinymist", "ts_ls", "rust-analyzer", "hls", "bashls", "pylsp", "markdown_oxide", "html",
+vim.lsp.enable({ "lua_ls", "tinymist", "ts_ls", "rust-analyzer", "bashls", "pylsp", "markdown_oxide", "html",
 	"jdtls", "tailwindcss", "emmet_ls" })
 vim.lsp.config("lua_ls", {
 	hint = {
