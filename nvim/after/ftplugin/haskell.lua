f55ade0f1360d602c0cfa4859d14fbc709810a49
Isak Hansen <isakhansen@hackerman.local>
2026-03-31 18:15:52 +0200

    haskell

--- nvim/after/ftplugin/haskell.lua
+++ nvim/after/ftplugin/haskell.lua
@@ -4,11 +4,10 @@ vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 
 vim.keymap.set('n', '<C-p><C-p>', function()
-	local filename = vim.fn.bufname('%')
-	os.execute("tmux split-window 'ghci " .. filename .. "; read'")
+	os.execute("tmux split-window 'cabal repl; read'")
 end, { silent = true, buffer = true })
 
 
 vim.keymap.set('n', '<C-p><C-o>', function()
-	os.execute("tmux split-window 'ghci *; read'")
+	vim.cmd("LspStart hls")
 end, { silent = true, buffer = true })
